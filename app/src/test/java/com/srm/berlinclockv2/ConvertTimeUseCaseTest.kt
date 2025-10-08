da44649954f5fba57540b69d3f6204045fbadd93
Ethan Hunt <ethanhunt1490@gmail.com>
2025-10-08 21:20:59 +0530

    Fix test cases for Zero to Four hours time

--- app/src/test/java/com/srm/berlinclockv2/ConvertTimeUseCaseTest.kt
+++ app/src/test/java/com/srm/berlinclockv2/ConvertTimeUseCaseTest.kt
@@ -66,31 +66,65 @@ class ConvertTimeUseCaseTest {
     fun testFiveMinuteRow_shouldHandleQuarterZeroMinute() {
         // 0 minutes
         val zeroMinuteState = useCase.execute(LocalTime.of(10,0,0))
+
         assertEquals("OOOOOOOOOOO", zeroMinuteState.fiveMinutesRow)
     }
 
     @Test
     fun testFiveMinuteRow_shouldHandleQuarterFifteenMinute() {
         val fifteenMinutesState = useCase.execute(LocalTime.of(10,15,0))
+
         assertEquals("YYROOOOOOOO", fifteenMinutesState.fiveMinutesRow)
     }
 
     @Test
     fun testFiveMinuteRow_shouldHandleQuarterThirtyMinute() {
         val thirtyMinutesState = useCase.execute(LocalTime.of(10,30,0))
+
         assertEquals("YYRYYROOOOO", thirtyMinutesState.fiveMinutesRow)
     }
 
     @Test
     fun testFiveMinuteRow_shouldHandleQuarterFiftyMinute() {
         val fiftyMinutesState = useCase.execute(LocalTime.of(10,50,0))
+
         assertEquals("YYRYYRYYRYO", fiftyMinutesState.fiveMinutesRow)
     }
 
     @Test
     fun testFiveMinuteRow_shouldHandleQuarterFiftyNineMinute() {
         val fiftyNineMinutesState = useCase.execute(LocalTime.of(10,59,0))
+
         assertEquals("YYRYYRYYRYY", fiftyNineMinutesState.fiveMinutesRow)
+    }
+
+    @Test
+    fun testOneHourRow_shouldHandleZeroHour() {
+        // 0 remainder - 0 lights blink
+        val zeroHourState = useCase.execute(LocalTime.of(10,0,0))
+
+        assertEquals("OOOO", zeroHourState.oneHoursRow)
+    }
+
+    @Test
+    fun testOneHourRow_shouldHandleThreeHour() {
+        val threeHourState = useCase.execute(LocalTime.of(3,0,0))
+
+        assertEquals("RRRO", threeHourState.oneHoursRow)
+    }
+
+    @Test
+    fun testOneHourRow_shouldHandleFourHour() {
+        val fourHourState = useCase.execute(LocalTime.of(4,0,0))
+
+        assertEquals("RRRR", fourHourState.oneHoursRow)
+    }
+
+    @Test
+    fun testOneHourRow_shouldHandleFiveHour() {
+        val fiveHourState = useCase.execute(LocalTime.of(5,0,0))
+
+        assertEquals("OOOO", fiveHourState.oneHoursRow)
     }
 
 }
